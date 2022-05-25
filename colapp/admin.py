9f80a724bf3b01d601a553f2e588466236e292b3
Riya <riya@Riyas-MacBook-Air.local>
2022-05-25 13:02:04 +0530

    initial commit

---
+++ colapp/admin.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+
+# Register your models here.
+from colapp.models import UserMember
+# from studentapp.models import course
+
+# register model
+
+admin.site.register(UserMember)
+# admin.site.regis
